5367d7459cd3ed78838e587b6d9b723008612b8f
Daniel Alejandro Cast <lexcast13@gmail.com>
2020-03-25 23:15:09 -0600

    Remove useless scripts

--- getColors.js
+++
@@ -1,9 +0,0 @@
-var Vibrant = require("node-vibrant");
-
-Vibrant.from("public/images/covers/8496284328.jpg").getPalette(
-  (err, palette) => {
-    console.log(palette.Vibrant.getHex());
-    console.log(palette.Vibrant.getTitleTextColor());
-    console.log(palette.Vibrant.getBodyTextColor());
-  }
-);
--- searchBook.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios").default;
-
-axios
-  .get("https://openlibrary.org/api/books?bibkeys=ISBN:8496284328&jscmd=data")
-  .then(r => {
-    console.log(r.data);
-  });
