0f9768f96bbc7320832fa060a73ebaa1624911b1
Daniel Alejandro Cast <lexcast13@gmail.com>
2020-04-02 17:36:20 -0600

    Improve perspective and alignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,37 +6,31 @@ const App = () => {
   const [selected, setSelected] = useState();
 
   return (
-    <div className="relative font-serif" style={{ width: "fit-content" }}>
-      <div
-        className="relative m-auto flex flex-col flex-wrap preserve-3d p-2"
-        style={{
-          perspective: "1800px",
-          perspectiveOrigin: "50% 15%",
-          transform: "scale(.6)"
-        }}
-      >
-        <div className="relative preserve-3d m-auto pt-20 px-20 flex justify-center items-baseline">
-          {books.map(book => (
-            <Book
-              key={book.isbn}
-              book={book}
-              selected={selected === book.isbn}
-              setSelected={setSelected}
-            />
-          ))}
-        </div>
-        <div
-          className="h-3 bg-wood preserve-3d"
-          style={{ transform: "translateZ(-1px)" }}
-        >
-          <div
-            className="h-64 bg-wood preserve-3d"
-            style={{
-              transform: "rotateX(-90deg)",
-              transformOrigin: "top"
-            }}
+    <div
+      className="relative font-serif m-auto flex flex-col flex-wrap preserve-3d p-2"
+      style={{ transform: "scale(.6)" }}
+    >
+      <div className="relative preserve-3d m-auto px-20 flex justify-center items-baseline">
+        {books.map(book => (
+          <Book
+            key={book.isbn}
+            book={book}
+            selected={selected === book.isbn}
+            setSelected={setSelected}
           />
-        </div>
+        ))}
+      </div>
+      <div
+        className="h-3 bg-wood preserve-3d"
+        style={{ transform: "translateZ(-1px)" }}
+      >
+        <div
+          className="h-64 bg-wood preserve-3d"
+          style={{
+            transform: "rotateX(-90deg)",
+            transformOrigin: "top"
+          }}
+        />
       </div>
     </div>
   );
