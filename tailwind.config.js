823af3adb2a1d8fe148c35377a85da6e1b364f0e
Daniel Alejandro Cast <lexcast13@gmail.com>
2020-05-19 01:46:26 -0500

    Upgrade deps & update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  theme: {
-    extend: {
-      backgroundColor: {
-        paper: "#f2eecb"
-      }
-    }
-  },
+  purge: ["./src/**/*.js", "./public/*.html"],
+  theme: { extend: { backgroundColor: { paper: "#f2eecb" } } },
   variants: {},
-  plugins: []
+  plugins: [],
 };
