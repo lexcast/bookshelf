aadb4814339dc59394b9af41cea8167370c41e2b
Daniel Alejandro Cast <lexcast13@gmail.com>
2020-05-22 01:33:21 -0500

    Updates

---
+++ precache-manifest.9a47a95ed6335d06f520a3033dbc4d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e196a9484ee01919378126b0f2e76d",
+    "url": "/bookshelf/index.html"
+  },
+  {
+    "revision": "8b98a23b2589d4225d82",
+    "url": "/bookshelf/static/css/main.06071c44.chunk.css"
+  },
+  {
+    "revision": "19bc1b56d118209ed0b2",
+    "url": "/bookshelf/static/js/2.f26627d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookshelf/static/js/2.f26627d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b98a23b2589d4225d82",
+    "url": "/bookshelf/static/js/main.e9d95052.chunk.js"
+  },
+  {
+    "revision": "cd50e4068402d38ebb43",
+    "url": "/bookshelf/static/js/runtime-main.65f16bb3.js"
+  }
+]);
